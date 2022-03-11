233ae81bbb70ed213287c2301147d7a2f8d13bf5
DyHon1 <lehoang8d@gmail.com>
2022-03-12 00:43:51 +0700

    full

---
+++ src/Components/Navigation/HeroText.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function HeroText() {
+  return (
+    <div className="hero-section-homepage">
+      <div className="hero-section-text">
+        <h1 className="mb-4">
+          {" "}
+          <span>MyLaptop</span> store
+        </h1>
+        <a href="/shop" className="shop-now-button">
+          Shop now
+        </a>
+      </div>
+    </div>
+  );
+}
